1555aaaaf78bd76a237e7f75e4e0a2e0ee40eb35
Alyssa Klopfenstein <alyssa.r.purdy@gmail.com>
2020-09-18 13:06:21 -0500

    modal js

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,19 +22,10 @@ var getWeatherByCity = function (city, isButtonClick) {
                 createListItems(cities);
             }
         }, function () {
-            // create a modal 
-            modal.style.display = "block";
-
-            var span = document.getElementsByClassName("close")[0];
-            span.onclick = function () {
-                modal.style.display = "none";
-            }
-            // When the user clicks anywhere outside of the modal, close it
-            window.onclick = function (event) {
-                if (event.target == modal) {
-                    modal.style.display = "none";
-                }
-            }
+            // modal
+            var el = document.querySelector('.modal');
+                var instance = M.Modal.init(el);
+                instance.open();
         });
 };
 
